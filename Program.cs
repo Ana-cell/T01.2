055f1d52986aa544b78019c5b1d7eac272977f5f
Ana-cell <70663224+Ana-cell@users.noreply.github.com>
2021-06-11 07:32:46 +0200

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Console.WriteLine("HOLA MUNDO!!!");
+            int num1 = 5;
+            int muero2 = 6;
+            double numero3 = 2.57;
+            double suma;
+            suma = num1 + numero3;
+
+            Console.WriteLine(suma);
+            string valor1 = Console.ReadLine();
+            Console.WriteLine("El número introducido es:" + valor1);
+            int valorint = Convert.ToInt32(valor1);
+
+            Console.ReadKey
+        }
+
+    }
+}
